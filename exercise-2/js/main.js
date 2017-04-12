2c333e908b8194908cde30195f0c4e6a206513dc
Michael Freeman <michael.k.freeman@gmail.com>
2017-04-12 15:46:59 -0700

    Revamped exercise-2

--- exercise-2/js/main.js
+++ exercise-2/js/main.js
@@ -1,60 +1,119 @@
-/* Create a scatter plot of 1960 life expectancy (gdp) versus 2013 life expectancy (life_expectancy).
-		The variable "data" is accessible to you, as you read it in from data.js
-*/
+/* Create a scatter plot of 1960 life expectancy (gdp) versus 2013 life expectancy (life_expectancy).*/
+
 $(function() {
-    // Read in your data. On success, run the rest of your code
+    // Graph margin settings
+    var margin = {
+        top: 10,
+        right: 10,
+        bottom: 150,
+        left: 60
+    };
+
+    // SVG width and height
+    var width = 960;
+    var height = 500;
+
+    // Graph width and height - accounting for margins
+    var drawWidth = width - margin.left - margin.right;
+    var drawHeight = height - margin.top - margin.bottom;
+
+    /************************************** Create chart wrappers ***************************************/
+    // Create a variable `svg` by selecting the element with id `vis` and appending an svg
+    // Set the width and height to your `width` and `height` variables
+    var svg = d3.select('#vis')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height);
+
+    // Append a `g` element to your svg in which you'll draw your bars. Store the element in a variable called `g`, and
+    // Transform the g using `margin.left` and `margin.top`
+    var g = svg.append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+        .attr('height', height)
+        .attr('width', width);
+
+    // Load data in using d3's csv function.
     d3.csv('data/prepped_data.csv', function(error, data) {
-        // Select SVG to work with, setting width and height (the vis <div> is defined in the index.html file)
-        var svg = d3.select('#vis')
-            .append('svg')
-            .attr('height', 600)
-            .attr('width', 600);
 
-        // Margin: how much space to put in the SVG for axes/titles
-        var margin = {
-            left: 70,
-            bottom: 100,
-            top: 50,
-            right: 50,
-        };
-
-        // Height/width of the chart itself
-        var height = 600 - margin.bottom - margin.top;
-        var width = 600 - margin.left - margin.right;
-
-        // 'g' element in which to place the circles, shifted down from the top left
-        var g = svg.append('g')
-            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-            .attr('height', height)
-            .attr('width', width);
-
-        // Find minimum and maximum values, then define x (log) and y (linear) scales
+        /************************************** Defining scales ***************************************/
+        // Find the maximum GDP value for the maximum of the x Scale, and multiply it by 1.05 (to add space)
         var xMax = d3.max(data, function(d) {
             return +d.gdp;
         }) * 1.05;
 
+        // Find the minimum GDP value for the minimum of the x Scale, and multiply it by .85 (to add space)
         var xMin = d3.min(data, function(d) {
             return +d.gdp;
         }) * 0.85;
 
-        var xScale = d3.scaleLog().range([0, width]).domain([xMin, xMax]);
+        // Use `d3.scaleLog` to define a variable `xScale` with the appropriate domain and range
+        var xScale = d3.scaleLog()
+            .range([0, drawWidth])
+            .domain([xMin, xMax]);
 
-        var yMin = d3.min(data, function(d) {
-            return +d.life_expectancy
-        }) * .9;
+        // Find the maximum life expectance value for the maximum of the y Scale, and multiply it by 1.05 (to add space)
         var yMax = d3.max(data, function(d) {
-            return +d.life_expectancy
+            return +d.life_expectancy;
         }) * 1.05;
 
-        var yScale = d3.scaleLinear().range([height, 0]).domain([yMin, yMax]);
+        // Find the minimum life expectance value for the minimum of the y Scale, and multiply it by .9 (to add space)
+        var yMin = d3.min(data, function(d) {
+            return +d.life_expectancy;
+        }) * 0.9;
 
-        // Perform a data-join for your data, creating circle element in your chart `g`
+        // Use `d3.scaleLinear` to define a variable `yScale` with the appropriate domain and range
+        var yScale = d3.scaleLinear()
+            .range([drawHeight, 0])
+            .domain([yMin, yMax]);
 
+        /************************************** Defining axes ***************************************/
+
+        // Define x axis using d3.axisBottom, assigning the scale as the xScale
+        var xAxis = d3.axisBottom()
+            .scale(xScale)
+            .ticks(5, 's');
+
+        // Define y axis using d3.axisLeft(), assigning the scale as the yScale
+        var yAxis = d3.axisLeft()
+            .scale(yScale)
+            .tickFormat(d3.format('.2s'));
+
+        /************************************** Rendering axes and labels ***************************************/
+
+        // Append a g to your SVG as an x axis label, specifying the 'transform' attribute to position it
+        // Make sure to use the `.call` method to render the axis in the label
+        var xAxisLabel = svg.append('g')
+            .attr('transform', 'translate(' + margin.left + ',' + (drawHeight + margin.top) + ')')
+            .attr('class', 'axis')
+            .call(xAxis);
+
+        // Append a g to your SVG as a y axis label, specifying the 'transform' attribute to position it
+        // Make sure to use the `.call` method to render the axis in the label
+        var yAxisLabel = svg.append('g')
+            .attr('class', 'axis')
+            .attr('transform', 'translate(' + margin.left + ',' + (margin.top) + ')')
+            .call(yAxis);
+
+        // Append a text element to your svg to label your x axis, and place it in the proper location
+        var xAxisText = svg.append('text')
+            .attr('transform', 'translate(' + (margin.left + drawWidth / 2) + ',' + (drawHeight + margin.top + 40) + ')')
+            .attr('class', 'title')
+            .text('Gross Domestic Product in 2014 (2005 USD)');
+
+        // Append a text element to your svg to label your y axis, and place it in the proper location
+        svg.append('text')
+            .attr('transform', 'translate(' + (margin.left - 40) + ',' + (margin.top + drawHeight / 2) + ') rotate(-90)')
+            .attr('class', 'title')
+            .text('Life expectancy in 2014');
+
+        /************************************** Data Join ***************************************/
         // Select all circles and bind data
         var circles = g.selectAll('circle').data(data);
 
-        // Use the .enter() method to get your entering elements, and assign initial positions
+        // Use the .enter() method to get your entering elements, and assign their positions
+        // Assign a 'title' property equal to the 'country' property (for hovers)
         circles.enter().append('circle')
+            .merge(circles)
             .attr('r', 10)
             .attr('fill', 'blue')
             .attr('cy', height)
@@ -72,51 +131,7 @@ $(function() {
         // Use the .exit() and .remove() methods to remove elements that are no longer in the data
         circles.exit().remove();
 
-        // Transition properties of the update selection
-        circles.transition()
-            .duration(1500)
-            .attr('cx', function(d) {
-                return xScale(d.gdp);
-            })
-            .attr('cy', function(d) {
-                return yScale(d.life_expectancy);
-            });
-
-        // Define x axis using d3.axisBottom, assigning the scale as the xScale
-        var xAxis = d3.axisBottom()
-            .scale(xScale)
-            .ticks(5, 's');
-
-        // Define y axis using d3.axisLeft(), assigning the scale as the yScale
-        var yAxis = d3.axisLeft()
-            .scale(yScale)
-            .tickFormat(d3.format('.2s'));
-
-        // Append x axis to your SVG, specifying the 'transform' attribute to position it
-        svg.append('g')
-            .attr('transform', 'translate(' + margin.left + ',' + (height + margin.top) + ')')
-            .attr('class', 'axis')
-            .call(xAxis);
-
-        // Append y axis to your SVG, specifying the 'transform' attribute to position it
-        svg.append('g')
-            .attr('class', 'axis')
-            .attr('transform', 'translate(' + margin.left + ',' + (margin.top) + ')')
-            .call(yAxis);
-
-        // Add a title g for the x axis
-        svg.append('text')
-            .attr('transform', 'translate(' + (margin.left + width / 2) + ',' + (height + margin.top + 40) + ')')
-            .attr('class', 'title')
-            .text('Gross Domestic Product in 2014 (2005 USD)');
-
-        // Add a title g for the x axis
-        svg.append('text')
-            .attr('transform', 'translate(' + (margin.left - 40) + ',' + (margin.top + height / 2) + ') rotate(-90)')
-            .attr('class', 'title')
-            .text('Life expectancy in 2014');
-
-        /* Using jQuery, select all circles and apply a tooltip
+        /* For easy hovers, let's use jQuery to select all circles and apply a tooltip
         If you want to use bootstrap, here's a hint:
         http://stackoverflow.com/questions/14697232/how-do-i-show-a-bootstrap-tooltip-with-an-svg-object
         */
